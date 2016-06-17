6e54341f8ee7c63ad2ac3f2f3b28241db65901f3
dem4ply <dem4ply@gmail.com>
2016-06-17 17:51:26 -0500

    configuaracion de compiz para wallpaper de video

---
+++ Documentos/script/wallpaper_vide.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This turns on xscreensaver using xwinwrap on one (or both) screens
+
+#SCREENCOUNT=$(xrandr -q | grep " connected " | grep -v " connected (" |  wc -l)
+SCREENS=$( xrandr -q | grep " connected " | awk '{ print $3 }' )
+SCREENS=$SCREENS | tr " " "\n"
+
+find ~/Videos/anime/ -type f >  /tmp/wallpaper_viedeo_playlist.txt
+#(xwinwrap -g 1920x1080+0 -ni -fs -o 1 -s -st -sp -b -nf -- mplayer -wid WID -quiet -loop 0 -ao null -shuffle <((find ~/Videos/anime -type f)) ) &
+#(xwinwrap -g 1366x768+1000 -ni -fs -o 1 -s -st -sp -b -nf -- mplayer -wid WID -quiet -loop 0 -ao null -shuffle <((find ~/Videos/anime -type f)) ) &
+( xwinwrap -g 1920x1080+0 -ni -fs -o 1 -s -st -sp -b -nf -- mplayer -wid WID -quiet -loop 0 -ao null -shuffle -playlist /tmp/wallpaper_viedeo_playlist.txt ) &
+sleep 1
+( xwinwrap -g +1500 -ni -fs -o 1 -s -st -sp -b -nf -- mplayer -wid WID -quiet -loop 0 -ao null -shuffle -playlist /tmp/wallpaper_viedeo_playlist.txt ) &
+
+#youtube-dl -f bestvideo[ext!=webm]+bestaudio[ext!=webm]/best[ext!=webm]  https://www.youtube.com/playlist?list=PLxCJsNWDGPvUwollYS7EbGLYdpAxp_u0g
