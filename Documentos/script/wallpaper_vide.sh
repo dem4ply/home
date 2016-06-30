4fbd2d212e50e17bb4a726e83431f6fee775ecc7
dem4ply <dem4ply@gmail.com>
2016-06-29 20:38:37 -0500

    se actualizo el compiz y el script para wallpaper

--- Documentos/script/wallpaper_vide.sh
+++ Documentos/script/wallpaper_vide.sh
@@ -2,10 +2,11 @@
 # This turns on xscreensaver using xwinwrap on one (or both) screens
 
 #SCREENCOUNT=$(xrandr -q | grep " connected " | grep -v " connected (" |  wc -l)
+killall xwinwrap
 SCREENS=$( xrandr -q | grep " connected " | awk '{ print $3 }' )
 SCREENS=$SCREENS | tr " " "\n"
 
-find ~/Videos/anime/ -type f >  /tmp/wallpaper_viedeo_playlist.txt
+find ~/Videos/anime/ -name "*.mp4" -type f >  /tmp/wallpaper_viedeo_playlist.txt
 #(xwinwrap -g 1920x1080+0 -ni -fs -o 1 -s -st -sp -b -nf -- mplayer -wid WID -quiet -loop 0 -ao null -shuffle <((find ~/Videos/anime -type f)) ) &
 #(xwinwrap -g 1366x768+1000 -ni -fs -o 1 -s -st -sp -b -nf -- mplayer -wid WID -quiet -loop 0 -ao null -shuffle <((find ~/Videos/anime -type f)) ) &
 ( xwinwrap -g 1920x1080+0 -ni -fs -o 1 -s -st -sp -b -nf -- mplayer -wid WID -quiet -loop 0 -ao null -shuffle -playlist /tmp/wallpaper_viedeo_playlist.txt ) &
